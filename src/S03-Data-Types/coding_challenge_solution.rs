5ff8eb5dd8551ebde0259d14bdbd2d0175ec96cd
Ramadan Yassin <ramadan.x@proton.me>
2025-06-21 11:51:35 +0300

    Initialize Rust project with Cargo configuration and add coding challenges for variables, data types, and main function.

---
+++ src/S03-Data-Types/coding_challenge_solution.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let visual_num: i32 = 1_337;
+    let new_visual_num = visual_num as i16;
+    let pi: f64 = 3.1234;
+    println!("{pi:.3}");
+    let with_milk = true;
+    let with_suger = false;
+    let is_my_type_of_coffee: bool = with_milk && with_suger;
+    let is_acceptable_coffee: bool = with_milk || with_suger;
+    let array_int: [i8; 4] = [1, 2, 3, 4];
+
+    println!("{array_int:?}");
+    
+    let student = ("Ramadan", 18, 3.5 );
+    let (name, age, gpa) = student;
+    println!("Student name {name}, age {age}, GPA {gpa}");
+    println!("{student:?}");
+
+
+
+
+}
