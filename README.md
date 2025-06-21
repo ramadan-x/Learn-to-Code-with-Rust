ca50e1d6a342ba5759ea8426560a8e49e4b45aae
Ramadan Yassin <ramadan.x@proton.me>
2025-06-21 12:25:47 +0300

    Update README.md with links to coding challenges and refactor Rust code for improved formatting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Learn-to-Code-with-Rust
 My solution for Learn to Code with Rust Udemy Course
+- [S02-Variables-and-Mutability](./src/S02-Variables-and-Mutability).
+- [S03-Data-Types](./src/S03-Data-Types).
--- src/S02-Variables-and-Mutability/coding_challenge_solution.rs
+++ src/S02-Variables-and-Mutability/coding_challenge_solution.rs
@@ -1,27 +1,25 @@
 fn main() {
-
     let season: &str = "Autumn";
 
     let mut points_scored: i32 = 28;
- 
+
     let team_name: &str = "Golden State Warriors";
 
- 
     const TOUCHDOWN_POINTS: i32 = 6;
- 
+
     let event_time: &str = "06:00";
 
     let event_time: i32 = 6;
- 
+
     println!(
         "My favorite season is {}, my team is {}, \
          I scored {} points, and the event time is {}. \
          A touchdown is worth {} points.",
         season, team_name, points_scored, event_time, TOUCHDOWN_POINTS
     );
-  
+
     let favorite_beverage: &str = "Coffee";
-  
+
     #[allow(unused_variables)]
     let favorite_beverage: &str = "Coffee";
 }
